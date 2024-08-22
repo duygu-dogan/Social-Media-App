17b4526ee74eb1938313dc93d72e4b33c7c2ffea
Duygu Doğan <duygudogan1509@gmail.com>
2024-08-23 02:04:31 +0300

    fix: exclude unnecessary files

--- SocialMediaApp/src/Application/TodoLists/Queries/GetTodos/TodosVm.cs
+++
@@ -1,9 +0,0 @@
-﻿using SocialMediaApp.Application.Common.Models;
-
-namespace SocialMediaApp.Application.TodoLists.Queries.GetTodos;
-public class TodosVm
-{
-    public IReadOnlyCollection<LookupDto> PriorityLevels { get; init; } = Array.Empty<LookupDto>();
-
-    public IReadOnlyCollection<TodoListDto> Lists { get; init; } = Array.Empty<TodoListDto>();
-}
