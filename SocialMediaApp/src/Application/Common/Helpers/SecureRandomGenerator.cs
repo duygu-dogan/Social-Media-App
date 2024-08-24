a31b90fe288e89a97fdd50eb7c385e4b3cdeb791
Duygu Doğan <duygudogan1509@gmail.com>
2024-08-24 18:18:43 +0300

    feat: secure random number generator

---
+++ SocialMediaApp/src/Application/Common/Helpers/SecureRandomGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Application.Common.Helpers;
+public class SecureRandomGenerator
+{
+    public int Next(int minValue, int maxValue)
+    {
+        if(minValue > maxValue)
+            throw new ArgumentOutOfRangeException(nameof(minValue), "minValue cannot be greater than maxValue");
+
+        int range = maxValue - minValue + 1;
+        byte[] randomBytes = new byte[4];
+
+        using(RandomNumberGenerator rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+
+        int randomValue = BitConverter.ToInt32(randomBytes, 0);
+        randomValue = Math.Abs(randomValue % range);
+
+        return randomValue + minValue;
+
+    }
+}
