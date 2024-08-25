58df2499ae713ee409aa9a22194d940cb2873b6a
Duygu Doğan <duygudogan1509@gmail.com>
2024-08-25 23:10:39 +0300

    feat: remove post like

---
+++ SocialMediaApp/src/Application/Likes/Commands/DeleteLike/DeleteLikeCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMediaApp.Application.Common.Interfaces;
+using SocialMediaApp.Domain.Entities;
+
+namespace SocialMediaApp.Application.Likes.Commands.DeleteLike;
+public class DeleteLikeCommandHandler : IRequestHandler<DeleteLikeCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteLikeCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteLikeCommand request, CancellationToken cancellationToken)
+    {
+        var like = await _context.Likes.FindAsync(request.id, cancellationToken);
+
+        Guard.Against.NotFound(request.id!, like);
+
+        _context.Likes.Remove(like);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
