839e6763ff4bf9702506df915d2e0e51cdb11d93
Duygu Doğan <duygudogan1509@gmail.com>
2024-08-25 22:56:32 +0300

    feat: post like

---
+++ SocialMediaApp/src/Application/Likes/Commands/CreateLike/CreateLikeCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Application.Likes.Commands.CreateLike;
+public record CreateLikeCommand: IRequest<Unit>
+{
+    public string? PostId { get; set; }
+}
---
+++ SocialMediaApp/src/Application/Likes/Commands/CreateLike/CreateLikeCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMediaApp.Application.Common.Exceptions;
+using SocialMediaApp.Application.Common.Interfaces;
+using SocialMediaApp.Domain.Entities;
+using SocialMediaApp.Domain.Events;
+
+namespace SocialMediaApp.Application.Likes.Commands.CreateLike;
+public class CreateLikeCommandHandler : IRequestHandler<CreateLikeCommand, Unit>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUser _user;
+
+    public CreateLikeCommandHandler(IApplicationDbContext context, IUser user)
+    {
+        _context = context;
+        _user = user;
+    }
+
+    public async Task<Unit> Handle(CreateLikeCommand request, CancellationToken cancellationToken)
+    {        
+        var post = await _context.Posts
+            .FirstOrDefaultAsync(p => p.Id.ToString() == request.PostId, cancellationToken);
+        if (post == null)
+            throw new NotFoundException(nameof(Post), request.PostId!);
+
+        var liker = await _context.DomainUsers
+            .FirstOrDefaultAsync(u => u.ApplicationUserId.ToString() == _user.Id, cancellationToken);
+        if (liker == null)
+            throw new ForbiddenAccessException();
+
+        var alreadyLiked = await _context.Likes.AnyAsync(l => l.PostId == post.Id && l.CreatedById == liker.Id, cancellationToken);
+        if (!alreadyLiked)
+        {
+            var entity = new Like
+            {
+                PostId = post.Id,
+                CreatedById = liker.Id
+            };
+
+            entity.AddDomainEvent(new LikeCreatedEvent(entity));
+
+            await _context.Likes.AddAsync(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        return Unit.Value;
+    }
+}
