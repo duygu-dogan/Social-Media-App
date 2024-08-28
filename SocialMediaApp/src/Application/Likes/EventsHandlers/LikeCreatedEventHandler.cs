324be269f9a8f2cece286070585729f3e20eb71d
Duygu Doğan <duygudogan1509@gmail.com>
2024-08-28 23:54:35 +0300

    feat: like created notification

---
+++ SocialMediaApp/src/Application/Likes/EventsHandlers/LikeCreatedEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SocialMediaApp.Application.Common.Interfaces;
+using SocialMediaApp.Domain.Entities;
+using SocialMediaApp.Domain.Events;
+
+namespace SocialMediaApp.Application.Likes.EventsHandlers;
+internal class LikeCreatedEventHandler : INotificationHandler<LikeCreatedEvent>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly ILogger<LikeCreatedEventHandler> _logger;
+
+    public LikeCreatedEventHandler(IApplicationDbContext context, ILogger<LikeCreatedEventHandler> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task Handle(LikeCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        var like = notification.Like;
+        
+        var alreadyNotified = await _context.Notifications.AnyAsync(
+            n => n.ForUserId == like!.Post!.CreatedById && n.CreatedById == like.CreatedById && n.Type == Domain.Entities.NotificationType.Like, cancellationToken);
+
+        if (alreadyNotified)
+            return;
+
+        var notif = new Notification
+        {   
+            ForUserId = like!.Post!.CreatedById,
+            Type = NotificationType.Like
+        };
+        await _context.Notifications.AddAsync(notif, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        _logger.LogInformation("SocialMediaApp Domain Event: {DomainEvent}", notification.GetType().Name);
+    }
+}
