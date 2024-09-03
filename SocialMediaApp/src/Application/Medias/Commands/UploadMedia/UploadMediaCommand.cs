2070c542dfcc853e745fec69b2b2dccf42fc6567
Duygu Doğan <duygudogan1509@gmail.com>
2024-09-03 04:13:40 +0300

    feat: Media upload

---
+++ SocialMediaApp/src/Application/Medias/Commands/UploadMedia/UploadMediaCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SocialMediaApp.Application.Medias.Commands.UploadMedia;
+internal class UploadMediaCommand:IRequest
+{
+    public string? PostId { get; set; }
+    public string? UserId { get; set; }
+    public string? FileName { get; set; }
+    public IFormFileCollection? Files { get; set; }
+}
---
+++ SocialMediaApp/src/Application/Medias/Commands/UploadMedia/UploadMediaCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMediaApp.Application.Common.Interfaces;
+using SocialMediaApp.Domain.Entities;
+
+namespace SocialMediaApp.Application.Medias.Commands.UploadMedia;
+internal class UploadMediaCommandHandler : IRequestHandler<UploadMediaCommand>
+{
+    private readonly IAzureStorageService _storage;
+    private readonly IApplicationDbContext _context;
+    private readonly IUser _user;
+
+    public UploadMediaCommandHandler(IAzureStorageService storage, IApplicationDbContext context, IUser userService)
+    {
+        _storage = storage;
+        _context = context;
+        _user = userService;
+    }
+
+    public async Task Handle(UploadMediaCommand request, CancellationToken cancellationToken)
+    {
+       if(request == null)
+            throw new ArgumentNullException(nameof(request));
+
+       var path = $"posts/{request.PostId}";
+       var files = request.Files;
+       if (files == null)
+            throw new ArgumentNullException(nameof(request.Files));
+       
+        var result = await _storage.UploadAsync(path, files);
+
+        await _context.Medias.AddRangeAsync(result.Select(x => new Media
+        {
+            FileName = x.fileName,
+            Path = x.path,
+            Post = _context.Posts.Find(request.PostId),
+            CreatedBy = _context.DomainUsers.Find(_user.Id)
+        }).ToList());
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
