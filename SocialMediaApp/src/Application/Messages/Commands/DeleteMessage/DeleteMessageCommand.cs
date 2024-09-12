955b6b14b6d8ec3bd6417d2f6b6a1065c26d9b7c
Duygu Doğan <duygudogan1509@gmail.com>
2024-09-12 23:12:34 +0300

    feat: delete message

---
+++ SocialMediaApp/src/Application/Messages/Commands/DeleteMessage/DeleteMessageCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Application.Messages.Commands.DeleteMessage;
+internal record DeleteMessageCommand(string? Id): IRequest
+{
+}
---
+++ SocialMediaApp/src/Application/Messages/Commands/DeleteMessage/DeleteMessageCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialMediaApp.Application.Common.Interfaces;
+using SocialMediaApp.Domain.Entities;
+
+namespace SocialMediaApp.Application.Messages.Commands.DeleteMessage;
+internal class DeleteMessageCommandHandler : IRequestHandler<DeleteMessageCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteMessageCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteMessageCommand request, CancellationToken cancellationToken)
+    {
+        var message = await _context.Messages.FindAsync(Guid.Parse(request.Id!));
+
+        Guard.Against.NotFound(request.Id!, message);
+
+        _context.Messages.Remove(message);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
