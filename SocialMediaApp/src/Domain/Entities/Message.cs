f111ef17975ce604cc8cabd9022bd8696c6a4130
Duygu Doğan <duygudogan1509@gmail.com>
2024-10-05 22:10:06 +0300

    fix: props fixed

--- SocialMediaApp/src/Domain/Entities/Message.cs
+++ SocialMediaApp/src/Domain/Entities/Message.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 namespace SocialMediaApp.Domain.Entities;
 public class Message:AuthorAuditableEntity
 {
-    public Guid FromUserId { get; set; }
-    public User? FromUser { get; set; }
+    public string? Content { get; set; }
     public Guid ToUserId { get; set; }
     public User? ToUser { get; set; }
-    public string? Content { get; set; }
-    public Media? Media { get; set; }
-    public Guid MediaId { get; set; }
+    public Guid ConversationId { get; set; }
+    public Conversation? Conversation { get; set; }
+    public bool IsRead { get; set; }
+    public Media? Media { get; set; } = null;
+    public Guid? MediaId { get; set; }
 }
