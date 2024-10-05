47f10a25795c6e09b3afe372e24939a0030c3f21
Duygu Doğan <duygudogan1509@gmail.com>
2024-10-05 22:07:10 +0300

    fix: required props added

--- SocialMediaApp/src/Application/Messages/Commands/CreateMessage/CreateMessageCommand.cs
+++ SocialMediaApp/src/Application/Messages/Commands/CreateMessage/CreateMessageCommand.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using SocialMediaApp.Domain.Entities;
 
 namespace SocialMediaApp.Application.Messages.Commands.CreateMessage;
-internal record CreateMessageCommand:IRequest
+public record CreateMessageCommand:IRequest
 {
     public string? ToUserId { get; set; }
     public string? Content { get; set; }
-    public string? MediaId { get; set; }
+    public string? ConversationId { get; set; }
+    public string? MediaId { get; set; } = null;
+    public bool IsRead { get; set; }
 }
