a66482757c69f460ce41cd0fc8eeb084a36dcba8
Duygu Doğan <duygudogan1509@gmail.com>
2024-10-05 22:14:16 +0300

    feat: mediaid foreign key added

---
+++ SocialMediaApp/src/Infrastructure/Data/Configurations/MessageConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaApp.Domain.Entities;
+
+namespace SocialMediaApp.Infrastructure.Data.Configurations;
+public class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasOne(m => m.Media)
+                .WithOne()
+                .HasForeignKey<Message>(m => m.MediaId)
+                .IsRequired(false); 
+    }
+}
