77776b7a1228f8f975da6decfce0bc65a5852338
Duygu Doğan <duygudogan1509@gmail.com>
2024-10-05 22:13:43 +0300

    fix: postid fixed to nullable

--- SocialMediaApp/src/Domain/Entities/Notification.cs
+++ SocialMediaApp/src/Domain/Entities/Notification.cs
@@ -10,7 +10,7 @@ public class Notification: AuthorAuditableEntity
     public User? ForUser { get; set; }
     public Guid ForUserId { get; set; }
     public Post? Post { get; set; }
-    public Guid PostId { get; set; }
+    public Guid? PostId { get; set; }
     public bool IsRead { get; set; }
     public NotificationType Type { get; set; }
 }
