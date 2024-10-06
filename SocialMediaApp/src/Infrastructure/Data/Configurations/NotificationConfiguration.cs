e4097697c78c1cd2b2b61c558c28a89ed37b8d42
Duygu Doğan <duygudogan1509@gmail.com>
2024-10-06 23:40:30 +0300

    feat: FK configuration added

---
+++ SocialMediaApp/src/Infrastructure/Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaApp.Domain.Entities;
+
+namespace SocialMediaApp.Infrastructure.Data.Configurations;
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.HasOne(n => n.CreatedBy)
+               .WithMany()
+               .HasForeignKey(n => n.CreatedById)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
